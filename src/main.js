b9cde704e48670e5340cce5750de359d38f866e9
Adrià Blancafort <adriablancafort@gmail.com>
2025-01-28 16:03:10 +0100

    load css from link

--- src/main.js
+++ src/main.js
@@ -5,4 +5,9 @@ import App from './App.svelte'
 const app = document.createElement('div');
 document.body.appendChild(app);
 
+const style = document.createElement('link');
+style.rel = 'stylesheet';
+style.href = chrome.runtime.getURL('styles.css'); 
+app.appendChild(style);
+
 mount(App, { target: app });
