642982c3852c3b8ae4e98d2547f2ef7a0c7ec99d
Adrià <adriablancafort@gmail.com>
2025-05-23 02:36:16 +0200

    add session id

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,10 +1,14 @@
+import { getSessionId } from "$lib/session.js";
+
 export async function getPrices() {
   const apiUrl = import.meta.env.VITE_API_URL;
   const currentUrl = window.location.href;
+  const sessionId = getSessionId();
 
   const response = await fetch(`${apiUrl}/v1/prices`, {
     method: 'GET',
     headers: {
+      'X-Session-ID': sessionId,
       'X-Product-URL': currentUrl
     }
   });
---
+++ src/lib/session.js
@@ -0,0 +1,4 @@
+export function getSessionId() {
+    const sessionId = "price-tracker-session-id";
+    return sessionId;
+}
