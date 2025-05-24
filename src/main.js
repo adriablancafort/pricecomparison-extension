005dd5f3e00eb0dce2e6854a74e8d5bbe9b26563
Adrià <adriablancafort@gmail.com>
2025-05-24 18:23:05 +0200

    rename variables

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { mount, unmount } from 'svelte';
 import './app.css';
-import Popup from './Popup.svelte';
+import App from './Popup.svelte';
 import { getPrices } from "$lib/external/main.js"; // in the future from CDN
 // const module = await import(CDN_URL);
 // const data = await module.getPrices(currentUrl);
@@ -25,19 +25,19 @@ if (import.meta.env.DEV) {
 }
 
 let currentUrl;
-let popup;
+let app;
 
 const observer = new MutationObserver(async () => {
   const newUrl = window.location.href;
   if (newUrl !== currentUrl) {
     currentUrl = newUrl;
 
-    // destroy previous popup instance
-    if (popup) unmount(popup);
+    // destroy previous app instance
+    if (app) unmount(app);
 
     // fetch the prices data and mount the app
     const data = await getPrices(currentUrl);
-    if (data) popup = mount(Popup, { target: shadowRoot, props: { data } });
+    if (data) app = mount(App, { target: shadowRoot, props: { data } });
   }
 });
 
