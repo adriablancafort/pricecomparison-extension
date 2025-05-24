c1d970e40164b33e39ec62f061ff1e0762a4fa61
Adrià <adriablancafort@gmail.com>
2025-05-24 18:20:11 +0200

    refactor to unmount popup more efficiently

--- src/main.js
+++ src/main.js
@@ -1,46 +1,43 @@
-import { mount } from 'svelte';
+import { mount, unmount } from 'svelte';
 import './app.css';
 import Popup from './Popup.svelte';
 import { getPrices } from "$lib/external/main.js"; // in the future from CDN
 // const module = await import(CDN_URL);
 // const data = await module.getPrices(currentUrl);
 
-async function init(data) {
-  // clear previous instance if any
-  document.querySelector('price-comparison')?.remove();
+const container = document.createElement('price-comparison');
+document.documentElement.appendChild(container);
 
-  const container = document.createElement('price-comparison');
-  document.documentElement.appendChild(container);
+const shadowRoot = container.attachShadow({ mode: 'open' });
 
-  const shadowRoot = container.attachShadow({ mode: 'open' });
+if (import.meta.env.PROD) {
+  const styles = document.createElement('link');
+  styles.rel = 'stylesheet';
+  styles.href = chrome.runtime.getURL('styles.css');
+  shadowRoot.appendChild(styles);
+}
 
-  if (import.meta.env.PROD) {
-    const styles = document.createElement('link');
-    styles.rel = 'stylesheet';
-    styles.href = chrome.runtime.getURL('styles.css');
-    shadowRoot.appendChild(styles);
-  }
-
-  if (import.meta.env.DEV) {
-    const styles = document.createElement('style');
-    const css = await import('./app.css?raw');
-    styles.textContent = css.default;
-    shadowRoot.appendChild(styles);
-  }
-
-  mount(Popup, { target: shadowRoot, props: { data } });
+if (import.meta.env.DEV) {
+  const styles = document.createElement('style');
+  const css = await import('./app.css?raw');
+  styles.textContent = css.default;
+  shadowRoot.appendChild(styles);
 }
 
 let currentUrl;
+let popup;
 
 const observer = new MutationObserver(async () => {
   const newUrl = window.location.href;
   if (newUrl !== currentUrl) {
     currentUrl = newUrl;
 
+    // destroy previous popup instance
+    if (popup) unmount(popup);
+
     // fetch the prices data and mount the app
     const data = await getPrices(currentUrl);
-    if (data) init(data);
+    if (data) popup = mount(Popup, { target: shadowRoot, props: { data } });
   }
 });
 
