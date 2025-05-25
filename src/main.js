a300c1e8c148971d6b82ba95269c7fd8b6b00a5c
Adrià <adriablancafort@gmail.com>
2025-05-25 14:35:23 +0200

    optimize mount and unmount cycles

--- src/main.js
+++ src/main.js
@@ -1,43 +1,51 @@
 import { mount, unmount } from 'svelte';
 import './app.css';
-import App from './Popup.svelte';
+import Popup from './Popup.svelte';
 import { getPrices } from "$lib/external/main.js"; // in the future from CDN
 // const module = await import(CDN_URL);
 // const data = await module.getPrices(currentUrl);
 
-const container = document.createElement('price-comparison');
-document.documentElement.appendChild(container);
+async function mountShadowRoot() {
+  const container = document.createElement('price-comparison');
+  document.documentElement.appendChild(container);
 
-const shadowRoot = container.attachShadow({ mode: 'open' });
+  shadowRoot = container.attachShadow({ mode: 'open' });
 
-if (import.meta.env.PROD) {
-  const styles = document.createElement('link');
-  styles.rel = 'stylesheet';
-  styles.href = chrome.runtime.getURL('styles.css');
-  shadowRoot.appendChild(styles);
+  if (import.meta.env.PROD) {
+    const styles = document.createElement('link');
+    styles.rel = 'stylesheet';
+    styles.href = chrome.runtime.getURL('styles.css');
+    shadowRoot.appendChild(styles);
+  } else {
+    const styles = document.createElement('style');
+    const css = await import('./app.css?raw');
+    styles.textContent = css.default;
+    shadowRoot.appendChild(styles);
+  }
 }
 
-if (import.meta.env.DEV) {
-  const styles = document.createElement('style');
-  const css = await import('./app.css?raw');
-  styles.textContent = css.default;
-  shadowRoot.appendChild(styles);
-}
-
+let shadowRoot;
+let popup;
 let currentUrl;
-let app;
 
 const observer = new MutationObserver(async () => {
   const newUrl = window.location.href;
   if (newUrl !== currentUrl) {
     currentUrl = newUrl;
 
-    // destroy previous app instance
-    if (app) unmount(app);
+    // wait for DOM to update
+    setTimeout(async () => {
 
-    // fetch the prices data and mount the app
-    const data = await getPrices(currentUrl);
-    if (data) app = mount(App, { target: shadowRoot, props: { data } });
+      // destroy previous popup instance
+      if (popup) unmount(popup);
+
+      // fetch data and mount popup
+      const data = await getPrices(currentUrl);
+      if (data) {
+        if (!shadowRoot) await mountShadowRoot();
+        popup = mount(Popup, { target: shadowRoot, props: { data } });
+      }
+    }, 10);
   }
 });
 
